3e50af5ec38b0262916aabcaa9db5baff9250f27
Richardlu0609 <richardlu0609@gmail.com>
2026-09-14 23:40:25 +0800

    Add github-pages/src/config.ts

---
+++ github-pages/src/config.ts
@@ -0,0 +1,6 @@
+export const SUPABASE_URL = "https://zrzcgyumxxqpkjrpfbyh.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_3-hih829hnclPYhI3fWsQg_akqY8-uf";
+
+export const APP_NAME = "衡鉴";
+export const APP_SUBTITLE = "线上项目评审评分系统";
+
